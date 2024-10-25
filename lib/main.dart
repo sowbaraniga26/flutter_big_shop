e197d41f6b0c6e74851a5c4843d89c3ecdafd106
sowbaraniga26 <sowbaraniga26@gmail.com>
2024-10-25 19:48:05 +0530

    Result Screenshot added

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: GenreScreen(title: 'Genre',)
-      home: BrandScreen(title: 'Brand',)
+      home: GenreScreen(title: 'Genre',)
     );
   }
 }
